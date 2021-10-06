3d57ddb254f437821466fb6d53d7acb08ee6cbf9
Mark Froehlich <mfroeh2@gmail.com>
2021-10-06 01:28:55 -0400

    basic CRUD operations for items

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,17 +1,85 @@
 const express = require('express');
 const router = express.Router();
-const items = require('../services/items');
+const { check, validationResult } = require('express-validator');
+
+const items = require( '../services/items' );
+
+const itemValidate = [
+    check( 'name', 'Name is a required field' ).isLength( { min: 2 }).trim().escape(),
+    check( 'description' ).trim().escape(),
+    check( 'purchased' ).trim().escape()
+  ]
 
 /* GET items. */
 router.get('/', async function(req, res, next) {
-  try {
-    res.json(await items.getItemsByUser(req.body.page, req.body.userId));
-  } catch ( err ) {
-      console.error( `Error while getting items `, err.message.message );
-    res.status(err.code).send(err.message)
+    try {
+        res.json(await items.getItemsByUser(req.query.page, req.query.userId));
+    } catch ( err ) {
+        console.error( `Error while getting items `, err.message );
+        res.status(err.code).send(err.message)
 
-    next(err);
-  }
-});
+        next(err);
+    }
+} );
+
+/* CREATE item. */
+router.post( '/', itemValidate, async function ( req, res, next ) {
+    try {
+        const errors = validationResult( req );
+        if ( !errors.isEmpty() ) {
+            return res.status( 422 ).json( { errors: errors.array() } );
+        }
+
+        const { name, description = '', purchased = false, userId } = req.body;
+        const item = {
+            name,
+            description,
+            purchased: purchased === '' ? false : purchased
+        }
+
+        res.json( await items.createItem( item, userId ) );
+    } catch ( err ) {
+        console.error( `Error while creating an item `, err.message );
+        res.status( err.code ).send( err.message )
+
+        next(err)
+    }
+} )
+
+/* UPDATE item */
+router.put( '/', itemValidate, async function ( req, res, next ) {
+    try {
+        const errors = validationResult( req );
+        if ( !errors.isEmpty() ) {
+            return res.status( 422 ).json( { errors: errors.array() } );
+        }
+
+        const { id, name, description = '', purchased = false } = req.body;
+        const item = {
+            id,
+            name,
+            description,
+            purchased: purchased === '' ? false : purchased
+        }
+
+        res.json( await items.updateItem( item ) );
+    } catch ( err ) {
+        console.error(`Error while updating an item `, err.message);
+        res.status(err.code).send(err.message);
+
+        next(err);
+    }
+} )
+
+router.delete( '/', async function ( req, res, next ) {
+    try {
+        res.json( await items.deleteItem( req.body.id ))
+    } catch ( err ) {
+        console.error(`Error while updating an item `, err.message);
+        res.status(err.code).send(err.message);
+
+        next(err);
+    }
+})
 
 module.exports = router;
--- backend/services/items.js
+++ backend/services/items.js
@@ -3,18 +3,71 @@ const util = require( '../util' );
 const config = require( '../config' );
 
 async function getItemsByUser ( page = 1, userId ) {
-    if ( !userId ) throw new Error( { code: 400, message: 'User ID not provided...' } )
+    if ( !userId ) throw { code: 400, message: 'User ID not provided...' }
 
     const offset = util.getOffset( page, config.itemsPerPage );
     const rows = await db.query(
-        `SELECT * FROM items WHERE user_id = ${userId} OFFSET $1 LIMIT $2`,
-        [offset, config.itemsPerPage]
+        `SELECT * FROM items WHERE user_id = $1 OFFSET $2 LIMIT $3`,
+        [userId, offset, config.itemsPerPage],
+        ( error, results ) => {
+            if ( error ) {
+                throw error
+            }
+        }
     )
     const data = util.emptyOrRows( rows );
 
     return { data }
 }
 
+async function createItem ( item, userId ) {
+    if ( !userId ) throw { code: 400, message: 'User ID not provided...' }
+
+    const { name, description = '', purchased = false } = item;
+    let result;
+    const query = await db.query(
+        `INSERT INTO items (name, description, user_id, purchased) VALUES ($1, $2, $3, $4)`,
+        [name, description, userId, purchased],
+        ( error, results ) => {
+            if ( error ) throw error;
+            result = { code: 200, message: 'Success' }
+        }
+    )
+
+    console.log('result: ', result)
+
+    return result
+}
+
+async function updateItem ( item ) {
+    if ( !item.id ) throw { code: 400, message: 'No item ID provided...' }
+
+    const { id, name, description = '', purchased = false } = item;
+
+    await db.query(
+        `UPDATE items SET name = $1, description = $2, purchased = $3 WHERE id = $4`,
+        [name, description, purchased, id],
+        ( error, results ) => { if (error) throw error }
+    )
+
+    return {id}
+}
+
+async function deleteItem ( itemId ) {
+    if ( !itemId ) throw { code: 400, message: 'No Item ID provided...' }
+
+    await db.query(
+        `DELETE FROM items WHERE id = $1`,
+        [itemId],
+        ( error ) => { if (error) throw error}
+    )
+
+    return `Item ${itemId} successfully deleted!`
+}
+
 module.exports = {
-    getItemsByUser
+    getItemsByUser,
+    createItem,
+    updateItem,
+    deleteItem
 }
